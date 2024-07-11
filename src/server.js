72613e58c041ac836b5c8d35eb5f10f380915cc3
NNASTYA2007 <nedyaknast@gmail.com>
2024-07-11 18:26:46 +0200

    fixing

--- src/server.js
+++ src/server.js
@@ -48,17 +48,17 @@ export const setupServer = () => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {**contactId**}!',
+      data: contact,
+    });
+
     if (!contact) {
       res.status(404).json({
         message: 'Contact not found',
       });
     }
     return;
-  });
-
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contact with id {**contactId**}!',
-    data: contact,
   });
 };
