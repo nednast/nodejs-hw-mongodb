848744e147deb131b4fa674ed3513bf458be6cc8
NNASTYA2007 <nedyaknast@gmail.com>
2024-07-14 13:15:05 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -46,18 +46,19 @@ export const setupServer = () => {
     }
     return;
   });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  // app.use((err, req, res, next) => {
+  //   res.status(500).json({
+  //     message: 'Something went wrong',
+  //     error: err.message,
+  //   });
+  // });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
   });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
