f5a48103b5005799880fe28afc0fe4e27394c720
NNASTYA2007 <nedyaknast@gmail.com>
2024-07-20 19:00:53 +0200

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -16,8 +16,8 @@ router.get('/contacts/:contactId', ctrlWrapper(getConactByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
 
-router.patch('/contacts/contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
-router.delete('/contacts/contactId', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
