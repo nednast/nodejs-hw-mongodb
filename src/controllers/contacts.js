a4fab23137425de804c571634a4a8397d39d469a
NNASTYA2007 <nedyaknast@gmail.com>
2024-07-20 18:45:41 +0200

    fixed

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -42,7 +42,7 @@ export const createContactController = async (req, res) => {
   });
 };
 
-export const patchContactController = async (req, res) => {
+export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body);
 
@@ -58,7 +58,7 @@ export const patchContactController = async (req, res) => {
   });
 };
 
-export const deleteContactController = async (req, res) => {
+export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await deleteContact(contactId);
 
